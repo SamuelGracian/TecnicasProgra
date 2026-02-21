569f55a25b0c4158ceed6d1ea732a6b7a0af24cb
samuel gracian <samuel.angeles641217@gmail.com>
2026-02-20 19:12:43 -0600

    Add file reading for shaders and fix width typo
    Added ReadFileToString utility for loading shader files, updated vertex shader creation to use file contents and defines. Fixed typo from 'widht' to 'width' in window and swap chain initialization. Added relevant includes and clarifying comments.

--- TecnicasProgra/src/Main/main.cpp
+++ TecnicasProgra/src/Main/main.cpp
@@ -1,5 +1,8 @@
 #include "Graphics/Dx11/DX11GraphicsAPI.h"
 #include <DirectXMath.h>
+#include <fstream>
+#include <string>
+#include <sstream> 
 
 
         
@@ -8,20 +11,44 @@ struct VERTEX
     float x, y, z;
     DirectX::XMFLOAT4 Color;
 };
+        
+std::vector<std::string> defines{ "#define TEST" };
+
+
+std::string ReadFileToString(const std::wstring& filePath)
+{
+    std::ifstream file(filePath, std::ios::in | std::ios::binary);
+
+    if (!file.is_open())
+    {
+        MessageBoxW(nullptr, L"Unable to open file", L"File Read Error", MB_OK);
+        return "";
+    }
+
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    file.close();
+
+    return buffer.str();
+}
 
 int main()
 {
-    uint32_t widht = 800;
+    uint32_t width = 800;
 
     uint32_t height = 600;
 
   std::shared_ptr<DisplaySurface> window = std::make_shared<DisplaySurface>();
-  window->init(widht,height, L"Tecnicas Progra");
+  window->init(width,height, L"Tecnicas Progra");
 
   std::shared_ptr<GRAPI> graphics = std::make_shared<DX11GraphicsAPI>();
   graphics->Init(window);
 
-  std::shared_ptr<SwapChain> P_swapChain = graphics->CreateSwapChain(window, widht,height, GAPI_FORMAT::FORMAT_R8G8B8A8_UNORM);
+  //Swap chain
+  std::shared_ptr<SwapChain> P_swapChain = graphics->CreateSwapChain(window, width,height, GAPI_FORMAT::FORMAT_R8G8B8A8_UNORM);
+
+  //vertex shader
+  std::shared_ptr<VertexShader> p_vertexShader = graphics->CreateVertexShader(ReadFileToString( L"Shaders.shaders"), "VS", defines);
 
   auto RTView = P_swapChain->GetRenderTargetView();
 
