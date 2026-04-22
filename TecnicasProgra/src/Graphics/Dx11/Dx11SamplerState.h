348a59f85bf101b0e76d3bbb355dc947714f1e9d
samuel gracian <samuel.angeles641217@gmail.com>
2026-04-22 12:43:02 -0600

    Dx11Sampler state class

---
+++ TecnicasProgra/src/Graphics/Dx11/Dx11SamplerState.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <d3d11.h>
+#include "Graphics/SamplerState.h"
+
+class Dx11SamplerState : public SamplerState
+{
+public:
+
+	Dx11SamplerState() : m_samplerState(nullptr){}
+
+	~Dx11SamplerState()
+	{
+		if (m_samplerState) m_samplerState->Release();
+	}
+
+
+private:
+	ID3D11SamplerState* m_samplerState;
+
+};
